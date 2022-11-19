1498b872d23f35ecee3bc82ac074606a5805e91e
Stephen Okyere <thuqpalmer@gmail.com>
2022-11-19 21:41:33 +0000

    feat:Add product container

---
+++ components/ProductsCon.js
@@ -0,0 +1,26 @@
+import styles from "../styles/Product.module.css";
+// import { urlFor } from "../lib/client";
+import Product from "./Product";
+
+const ProductsCon = ({ productSection }) => {
+  console.log(productSection);
+  return (
+    <div className={styles.wrapper}>
+      <h1>CHECK OUT OUR BESTSELLERS</h1>
+      <div className={styles.container}>
+        {productSection.map((product) => (
+          <Product
+            key={product.name}
+            name={product.name}
+            link={product.slug.current}
+            price={product.price.toFixed(2)}
+            src={product.image[0]}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+//    <Product name={product.name} price={product.price} src={product.image} />;
+
+export default ProductsCon;
